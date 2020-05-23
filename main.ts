51db522815dd876fc5da1d8e5eb76635542c9199
Alexandre Dumont aka Adlx <adumont@users.noreply.github.com>
2020-05-23 19:31:54 +0200

    Put the example in main so it renders in the Readme

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+let vLight = 0
+let sensors: number[] = []
+let vTemp = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(vLight)
+    ADTools.logNumber(vLight)
+})
+radio.onReceivedString(function (receivedString) {
+    sensors = ADTools.unpackNums(receivedString, Delimiters.Pipe)
+    vLight = sensors[0]
+    vTemp = sensors[1]
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(vTemp)
+    ADTools.logNumber(vTemp)
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString(ADTools.packNums([input.lightLevel(), input.temperature()], Delimiters.Pipe))
+})
